32b5f6b850d3ccd451cf2fd4fd34271a80205847
Irina Lukina <ibenderina@gmail.com>
2020-02-22 19:27:09 +0300

    15. Личный проект: надо подкачаться (часть 1)

--- js/upload.js
+++ js/upload.js
@@ -1,12 +1,39 @@
 'use strict';
 
 (function () {
-  var imgUploadPreview = document.querySelector('.img-upload__preview');
+  var imgUploadPreviewWrapper = document.querySelector('.img-upload__preview');
+  var imgUploadPreview = imgUploadPreviewWrapper.querySelector('img');
   var imgUploadOverlay = document.querySelector('.img-upload__overlay');
   var imgUploadCancel = document.querySelector('.img-upload__cancel');
   var uploadFile = document.querySelector('#upload-file');
   var imgUploadForm = document.querySelector('.img-upload__form');
   var imgUploadSubmit = document.querySelector('.img-upload__submit');
+
+  var onSetupSubmitClick = function (evt, form) {
+    evt.preventDefault();
+    var data = new FormData(form);
+    window.backend.save(data, function () {
+      window.utilities.onCloseWindowClick(imgUploadOverlay);
+    }, window.upload.onPictureLoadError);
+  };
+
+  var onSetupSubmitEnterKeydown = function (evt, form) {
+    if (evt.key === window.utilities.ENTER_KEY) {
+      onSetupSubmitClick(evt, form);
+    }
+  };
+
+  var onPictureLoadError = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
 
   uploadFile.addEventListener('change', function () {
     imgUploadOverlay.classList.remove(window.utilities.HIDDEN_CLASS);
@@ -29,15 +56,18 @@
 
   imgUploadSubmit.addEventListener('click', function (evt) {
     if (imgUploadForm.checkValidity()) {
-      window.utilities.onSetupSubmitClick(evt, imgUploadForm);
+      onSetupSubmitClick(evt, imgUploadForm);
     }
   });
 
   imgUploadSubmit.addEventListener('keydown', function (evt) {
     if (imgUploadForm.checkValidity()) {
-      window.utilities.onSetupSubmitEnterKeydown(evt, imgUploadForm);
+      onSetupSubmitEnterKeydown(evt, imgUploadForm);
     }
   });
 
-  window.imgUploadPreview = imgUploadPreview;
+  window.upload = {
+    imgUploadPreview: imgUploadPreview,
+    onPictureLoadError: onPictureLoadError
+  };
 })();
