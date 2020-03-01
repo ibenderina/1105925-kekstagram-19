9034568b49b8390fa6c53f96959ad6c30f63ca57
Irina Lukina <ibenderina@gmail.com>
2020-03-01 16:01:02 +0300

    18. Личный проект: перламутровые пуговицы (часть 1)

--- js/hashtags.js
+++ js/hashtags.js
@@ -1,38 +1,43 @@
 'use strict';
 
 (function () {
-  var ERROR_AMOUNT = 'Нельзя указать больше пяти хэш-тегов';
-  var ERROR_FIRST_SYMBOL = 'Укажите символ # в начале хэштега';
-  var ERROR_LENGTH = 'Длина хэштега должна быть от 1 до 20 символов';
-  var ERROR_SYMBOLS = 'Строка после решётки должна состоять только из букв и чисел';
-  var ERROR_REPEAT = 'Нельзя использовать один и тот же хэштег два и более раз';
-  var MAX_LENGTH = 5;
-  var MIN_HASHTAG_LENGTH = 1;
-  var MAX_HASHTAG_LENGTH = 20;
+  var Error = {
+    AMOUNT: 'Нельзя указать больше пяти хэш-тегов',
+    FIRST_SYMBOL: 'Укажите символ # в начале хэштега',
+    LENGTH: 'Длина хэштега должна быть от 1 до 20 символов',
+    SYMBOLS: 'Строка после решётки должна состоять только из букв и чисел',
+    REPEAT: 'Нельзя использовать один и тот же хэштег два и более раз'
+  };
+  var Length = {
+    MAX: 5,
+    MIN_HASHTAG: 1,
+    MAX_HASHTAG: 20
+  };
+
   var FIRST_SYMBOL = '#';
   var SYMBOL_PATTERN = new RegExp('[\\d\\wа-я]', 'ug');
 
   var textHashtags = document.querySelector('.text__hashtags');
 
   var hashtagsValidation = function (hashtags) {
-    if (hashtags.length > MAX_LENGTH) {
-      return ERROR_AMOUNT;
+    if (hashtags.length > Length.MAX) {
+      return Error.AMOUNT;
     }
 
     for (var i = 0; i < hashtags.length; i++) {
       var hashtag = hashtags[i];
       if (hashtag[0] !== FIRST_SYMBOL) {
-        return ERROR_FIRST_SYMBOL;
+        return Error.FIRST_SYMBOL;
       }
-      if (hashtag.length <= MIN_HASHTAG_LENGTH || hashtag.length > MAX_HASHTAG_LENGTH) {
-        return ERROR_LENGTH;
+      if (hashtag.length <= Length.MIN_HASHTAG || hashtag.length > Length.MAX_HASHTAG) {
+        return Error.LENGTH;
       }
       var hashtagValidator = hashtag.match(SYMBOL_PATTERN) || [];
       if (hashtag.slice(1) !== hashtagValidator.join('')) {
-        return ERROR_SYMBOLS;
+        return Error.SYMBOLS;
       }
       if (hashtags.indexOf(hashtag, i + 1) !== -1) {
-        return ERROR_REPEAT;
+        return Error.REPEAT;
       }
     }
     return '';
